8a1b25119d2562401a9458aa5ec1231235dcc861
polinkavishinka <43956791+polinkavishinka@users.noreply.github.com>
2018-10-08 15:28:43 +0300

    2 глава 1 задание прат

---
+++ прат/2 глава/1 задание/2 глава прат 1 задание.cpp
@@ -0,0 +1,14 @@
+// ConsoleApplication8.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(0, "rus");
+	cout << " Вязанкина Полина" << endl << "polinkavishinka@mail.ru";
+	system("pause");
+	return 0;
+}
