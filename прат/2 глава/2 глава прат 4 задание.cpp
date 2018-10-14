e07f95b31fada173b28bd29f047efe8583b6204e
polinkavishinka <43956791+polinkavishinka@users.noreply.github.com>
2018-10-14 18:23:41 +0300

    2 глава прат 4 задание

--- прат/2 глава/2 глава прат 4 задание.cpp
+++ прат/2 глава/2 глава прат 4 задание.cpp
@@ -10,16 +10,17 @@ void text()
 
 
 	float a, d;
-	setlocale(0, "rus");
 	cout << "введите значение а в фаренгейтах" << endl;
 	cin >> a;
 	cout << "значение в фаренгейтах" << endl;
 	d = (1,8*a)+32;
 	cout << d << "значение в цельсиях";
+	system("pause");
 }
 
 int main()
 {
+	setlocale(0, "rus");
 	text();
 	
     return 0;
